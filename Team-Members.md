df707d45d5b7963332cbb6e4e9807ebf99521986
Okibe Abang <okibe.abang@ucalgary.ca>
2023-03-24 11:16:42 -0600

    Lotion website added

--- Team-Members.md
+++ Team-Members.md
@@ -2,5 +2,5 @@
 
 Put your names and UCIDs here:
 
-- Member #1: Name -- UCID
+- Member #1: Okibe Abang -- 30162192
 - Member #2: Name -- UCID
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Layout from "./Layout";
+import WriteBox from "./WriteBox";
+import Empty from "./Empty";
+import reportWebVitals from "./reportWebVitals";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+ReactDOM.render(
+  <GoogleOAuthProvider clientId="386932037035-k8v833noqjk7m4***********.apps.googleusercontent.com">
+      <React.StrictMode>
+          <App />
+      </React.StrictMode>
+  </GoogleOAuthProvider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Empty />} />
+          <Route path="/notes" element={<Empty />} />
+          <Route
+            path="/notes/:noteId/edit"
+            element={<WriteBox edit={true} />}
+          />
+          <Route path="/notes/:noteId" element={<WriteBox edit={false} />} />
+          {/* any other path */}
+          <Route path="*" element={<Empty />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </>
+);
+
 reportWebVitals();
