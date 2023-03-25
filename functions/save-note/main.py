b4128452dbf70d76a74fd13254ecd7e098ab06c9
OkibeAbang <113947911+OkibeAbang@users.noreply.github.com>
2023-03-25 11:37:53 -0600

    Update main.py

--- functions/save-note/main.py
+++ functions/save-note/main.py
@@ -1,5 +1,5 @@
 # add your save-note function here
-import json
+import json 
 import boto3
 
 dynamodb_resource = boto3.resource("dynamodb")
@@ -7,18 +7,19 @@ table = dynamodb_resource.Table("lotion-30140722")
 
 def lambda_handler(event, context):
     body = json.loads(event["body"])
-    try:
-        table.put_item(Item=body)
-        return{
-            "StatusCode": 201,
-            "body": "success"
+    try: 
+        table.put_item(Item = body)
+        return {
+            "statusCode": 201,
+                "body":json.dumps({
+                     "message": "success"
+                })
         }
     except Exception as exp:
-        return{
-            "StatusCode": 500,
-            "body": json.dumps(
-            {
-            "message:": str(exp)
-            }
-            )
+        print(f"exception: {exp}")
+        return {
+            "statusCode": 500,
+                "body": json.dumps({
+                    "message":str(exp)
+            })
         }
