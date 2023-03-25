97e4cf6e845ccc01910e37275b793f8d827e7b2a
OkibeAbang <113947911+OkibeAbang@users.noreply.github.com>
2023-03-25 11:41:55 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
-import Layout from "./Layout";
-import WriteBox from "./WriteBox";
-import Empty from "./Empty";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
-
-
-
-
+import reportWebVitals from "./reportWebVitals";
+import Layout from './Layout';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <>
-    <GoogleOAuthProvider clientId="386932037035-k8v833noqjk7m4***********.apps.googleusercontent.com">
-      <BrowserRouter>
-        <Routes>
-          <Route element={<App />}>
-            <Route path="/" element={<Empty />} />
-            <Route path="/notes" element={<Empty />} />
-            <Route
-              path="/notes/:noteId/edit"
-              element={<WriteBox edit={true} />}
-            />
-            <Route path="/notes/:noteId" element={<WriteBox edit={false} />} />
-            {/* any other path */}
-            <Route path="*" element={<Empty />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </GoogleOAuthProvider>
-  </>
+  <GoogleOAuthProvider clientId="586690601913-4us54n8tk6j3luiliskv9dqaeiluvijk.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 reportWebVitals();
