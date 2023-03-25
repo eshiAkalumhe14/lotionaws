4392134708995d931d8cc38239b35d38a99fc478
OkibeAbang <113947911+OkibeAbang@users.noreply.github.com>
2023-03-25 11:42:41 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,36 @@
-/*App.js*/
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Layout from "./Layout";
+import WriteBox from "./WriteBox";
+import Empty from "./Empty";
+import Login from "./Login";
 import React, { useState, useEffect } from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
-import Layout from './Layout';
+import NoteItem from "./NoteItem";
+import addNote from "./Layout";
+import setNotes from "./Layout";
 
 function App() {
-    const [ user, setUser ] = useState([]);
-    const [ profile, setProfile ] = useState([]);
-
-    const login = useGoogleLogin({
-        onSuccess: (codeResponse) => setUser(codeResponse),
-        onError: (error) => console.log('Login Failed:', error)
-    });
-
-    useEffect(
-        () => {
-            if (user) {
-                axios
-                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                        headers: {
-                            Authorization: `Bearer ${user.access_token}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                    .then((res) => {
-                        setProfile(res.data);
-                    })
-                    .catch((err) => console.log(err));
-            }
-        },
-        [ user ]
-    );
-
-    // log out function to log the user out of google and set the profile array to null
-    const logOut = () => {
-        googleLogout();
-        setProfile(null);
-    };
 
     return (
-        <div>
-            <h2>React Google Login</h2>
-            <br />
-            <br />
-            {profile ? (
-                <div>
-                    <img src={profile.picture} alt="user image" />
-                    <h3>User Logged in</h3>
-                    <p>Name: {profile.name}</p>
-                    <p>Email Address: {profile.email}</p>
-                    <br />
-                    <br />
-                    <Layout />
-                    <button onClick={logOut}>Log out</button>
-                </div>
-            ) : (
-                <button onClick={() => login()}>Sign in with Google 🚀 </button>
-            )}
-        </div>
-    );
-}
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Empty />} />
+              <Route path="/notes" element={<Empty />} />
+              <Route
+                path="/notes/:noteId/edit"
+                element={<WriteBox edit={true} />}
+              />
+              <Route path="/notes/:noteId" element={<WriteBox edit={false} />} />
+              {/* any other path */}
+              <Route path="*" element={<Empty />} />
+            </Route>
+            <Route path = "/login" element  = {<Login />}></Route>
+          </Routes>
+        </BrowserRouter>
+        )
+}   
+
 export default App;
